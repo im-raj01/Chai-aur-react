27a4e376eaefa3b796cf54c3e5b8991eee9accd7
im-raj01 <aaditya.helloworld@gmail.com>
2024-01-29 13:21:39 +0530

    create-react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Hello World (welcome to Chai)</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+
+
+  return (
+   <h1>Hii Chai aur react with Vite | Aditya</h1>
+  )
+}
+
+export default App
