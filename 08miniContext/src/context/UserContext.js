be9bbf7acf948121a299ddec791a574879efb44c
im-raj01 <aaditya.helloworld@gmail.com>
2024-03-18 13:42:51 +0530

    themeSwitcher

--- 08miniContext/src/context/UserContext.js
+++ 08miniContext/src/context/UserContext.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 const UserContext = React.createContext()
+//har ek context ek provider hota hai to usercontext bhi ek provider hoga.
 
 export default UserContext;
+
+
+
+{/* <UserContext>
+<Login />
+<Card>
+    <Data />
+</Card>
+<UserContext/> */}
